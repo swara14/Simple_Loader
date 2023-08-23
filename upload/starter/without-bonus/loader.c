e09fdf97e0f7f498f7c544295e533d92b48174d4
shrutya22487 <131897020+shrutya22487@users.noreply.github.com>
2023-08-23 20:09:09 +0530

    Update loader.c

--- upload/starter/without-bonus/loader.c
+++ upload/starter/without-bonus/loader.c
@@ -1,55 +1,31 @@
 #include "loader.h"
-#include <stdbool.h>
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
-
 int fd , i ,min_entrypoint;
-void *entry_pt = NULL , *virtual_mem = NULL;
+Elf32_Addr entry_pt = 0 ;
+void *virtual_mem = NULL;
 
 /*
  * release memory and other cleanups
  */
-
-bool check_file_read(const char* exe){
-// Open the ELF file for reading
-  int fd = open(exe, O_RDONLY);
-  if (fd < 0) {
-    perror("Error opening ELF file");
-    return false;
-  }
-  return true;
-}
-
-void exit_program(size_t bytes_received){
-  if (bytes_received == -1)
-  {
-    exit(0);
-  }
-  return;  
-}
-
-// Loader cleanup 
-// Free memory allocation for ehdr and phdr
 void free_space(){
     free(ehdr);
     free(phdr);
 }
 
-void unmapping_virtual_memory(){
-    if (virtual_mem != NULL) {
-        munmap(virtual_mem, phdr[0].p_memsz);
-    }
-
-  // Closing the file descriptor if it was opened
-    close(fd);
-}
-
-void checking_offset_1( off_t new_position ){
-  if ( new_position == -1 )
-  {
-    printf("Not able to seek\n");
+void check_file_read(const char* exe){
+  int fd = open(exe, O_RDONLY);
+  if (fd < 0) {
+    printf("Error opening ELF file\n");
     exit(1);
   }
+}
+
+void unmapping_virtual_memory(){
+    if (virtual_mem != NULL) {
+        munmap(virtual_mem, phdr[i].p_memsz);
+    }
+    close(fd);
 }
 
 void check_offset( off_t new_position ){
@@ -59,6 +35,49 @@ void check_offset( off_t new_position ){
     exit(1);
   }
 }
+
+void load_phdr( size_t size_of_phdr ){
+  phdr = ( Elf32_Phdr* )malloc( size_of_phdr * ehdr->e_phnum); 
+  
+  if (phdr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(1);
+  }
+  
+  check_offset(lseek(fd, 0, SEEK_SET));
+  check_offset( lseek(fd , ehdr -> e_phoff , SEEK_SET ) );
+  
+  if ( read( fd , phdr , size_of_phdr * ehdr -> e_phnum) !=  size_of_phdr * ehdr -> e_phnum)
+  {
+    printf("memory not allocated properly to phdr\n");
+    exit(1);
+  }
+  return;
+}
+
+void load_ehdr( size_t size_of_ehdr ){
+  ehdr = ( Elf32_Ehdr* )malloc(size_of_ehdr);
+  
+  if (ehdr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(1);
+  }
+
+  check_offset( lseek(fd, 0, SEEK_SET) ); 
+  if (read(fd, ehdr, size_of_ehdr) != size_of_ehdr)
+  {
+    printf("Memory not allocated properly to ehdr\n");
+    exit(1);
+  }
+  if (ehdr -> e_ident[EI_CLASS] != ELFCLASS32) {
+    printf("Not a 32-bit ELF file\n");
+    exit(1);
+  }
+  return;
+}
+/*
+ * Load and run the ELF executable file
+ */
 
 void find_entry_pt(){
   i = 0  ;
@@ -76,49 +95,40 @@ void find_entry_pt(){
     }
   }
   i = min_entrypoint;
-  entry_pt = &phdr[i];
+  entry_pt = phdr[i].p_vaddr;
 }
 
 void Load_memory(){
   virtual_mem = mmap(NULL, phdr[i].p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE , 0, 0);  
+  
   if (virtual_mem == MAP_FAILED) {
       printf("Error allocating virtual memory\n");
       exit(1);
   }
   
   check_offset(lseek(fd, 0, SEEK_SET));
-  check_offset( lseek( fd , phdr[1].p_offset ,SEEK_SET ) );
+  check_offset( lseek( fd , phdr[i].p_offset ,SEEK_SET ) );
 
-  read(fd , virtual_mem ,phdr[1].p_memsz) ;
+  read(fd , virtual_mem ,phdr[i].p_memsz) ;
 }
-/*
- * Load and run the ELF executable file
- */
+
+void open_elf( char* exe ){
+  fd = open(exe, O_RDONLY);
+  
+  if (fd < 0) {
+    printf("Bad file Descriptor\n");
+    exit(1);
+  }
+}
+
 void load_and_run_elf(char* exe) {
 
-  fd = open(exe, O_RDONLY);
   size_t size_of_phdr = sizeof( Elf32_Phdr ) ,size_of_ehdr = sizeof( Elf32_Ehdr); // of one segment
 
   // 1. Load entire binary content into the memory from the ELF file.
 
-  ehdr = ( Elf32_Ehdr* )malloc(size_of_ehdr);
-  if (ehdr == NULL) {
-        printf("Memory not allocated.\n");
-        exit(0);
-  }
-
-  check_offset( lseek(fd, 0, SEEK_SET) ); 
-  exit_program( read(fd, ehdr, size_of_ehdr) );
-
-  phdr = ( Elf32_Phdr* )malloc( size_of_phdr * ehdr->e_phnum); 
-  if (phdr == NULL) {
-        printf("Memory not allocated.\n");
-        exit(0);
-  }
-  
-  check_offset(lseek(fd, 0, SEEK_SET));
-  check_offset( lseek(fd , ehdr -> e_phoff , SEEK_SET ) );
-  exit_program(read( fd , phdr , size_of_phdr * ehdr -> e_phnum));
+  load_ehdr( size_of_ehdr );
+  load_phdr( size_of_phdr );
 
   // 2. Iterate through the PHDR table and find the section of PT_LOAD type that contains the address of the entrypoint method in fib.c
 
@@ -127,15 +137,15 @@ void load_and_run_elf(char* exe) {
   // 3. Allocate memory of the size "p_memsz" using mmap function and then copy the segment content
 
   Load_memory();
-  
+
   // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step  
-  
-  void *entry_virtual = virtual_mem + ehdr-> e_entry - phdr[i].p_vaddr;
+  Elf32_Addr offset = ehdr->e_entry - entry_pt;   // offset between entry point address and starting address of the PT_LOAD segment.
+
+  void *entry_virtual = virtual_mem + offset;  // actual memory address of the _start function in loaded memory.
 
   // 5. Typecast the address to that of a function pointer matching "_start" method in fib.c.
-
-  typedef int (*start)();
-  start _start = (start)(entry_virtual);
+  
+  int (*_start)() = (int(*)())entry_virtual;
 
   // 6. Call the "_start" method and print the value returned from the "_start"
   
@@ -143,34 +153,7 @@ void load_and_run_elf(char* exe) {
 
   munmap(virtual_mem, phdr->p_memsz);
   printf("User _start return value = %d\n",result);
-  
-}
 
-bool check_file_read(const char* exe){
-// Open the ELF file for reading
-  int fd = open(exe, O_RDONLY);
-  if (fd < 0) {
-    perror("Error opening ELF file");
-    return false;
-  }
-  return true;
-}
-
-bool check_read_elf(const char* exe){
-// Read the ELF header
-  Elf32_Ehdr ehdr;
-  if (read(fd, &ehdr, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr)) {
-    perror("Error reading ELF header");
-    close(fd);
-    return false;
-  }
-
-  if (ehdr.e_ident[EI_CLASS] != ELFCLASS32) {
-    fprintf(stderr, "Not a 32-bit ELF file\n");
-    close(fd);
-    return false;
-  }
-  return true;
 }
 
 int main(int argc, char** argv) 
@@ -180,16 +163,16 @@ int main(int argc, char** argv)
     exit(1);
   }
   // 1. carry out necessary checks on the input ELF file
-  if (!check_file_read(argv[1]) && !check_file_read(argv[1])) {
-      fprintf(stderr, "Failed to pass the ELF file checks\n");
-      exit(1);
-    }
+  open_elf( argv[1] );
 
   // 2. passing it to the loader for carrying out the loading/execution
+
   load_and_run_elf(argv[1]);
-  
+
   // 3. invoke the cleanup routine inside the loader  
+
   free_space();
   unmapping_virtual_memory();
+
   return 0;
 }
